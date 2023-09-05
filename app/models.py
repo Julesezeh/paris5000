d3466e916d572f264fae2fe4ba35b2fa0639b5b8
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-05 14:37:47 +0100

    compromised

--- app/models.py
+++
@@ -1,6 +0,0 @@
-from extensions import db
-
-class Users(db.Model):
-    username  = db.Column(db.String(100))
-    name = db.Column(db.String(200))
-    # date_of_birth = db.Column(db)....date field
